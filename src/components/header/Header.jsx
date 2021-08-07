1c16e6225a592780abbc6f99c438554aea3ef23c
Nikita <no.colour.you.like@gmail.com>
2021-08-07 15:19:22 +0300

    link fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../../firebase/firebase.utils";
-import { connect } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
-import "./header.scss";
-import Logo from "../../img/logo.jpg";
-import Cart from "../cart/Cart";
+import './header.scss';
+import Logo from '../../img/logo.jpg';
+import Cart from '../cart/Cart';
 
 const Header = ({ currentUser }) => {
   return (
-    <div className="header">
-      <Link className="logo-container" to="/plants-e-commerce">
-        <img className="logo-img" src={Logo} alt="logo-img" />
+    <div className='header'>
+      <Link className='logo-container' to='/plants-e-commerce/'>
+        <img className='logo-img' src={Logo} alt='logo-img' />
       </Link>
-      <div className="menu">
-        <Link className="menu-link" to="/plants-e-commerce">
+      <div className='menu'>
+        <Link className='menu-link' to='/plants-e-commerce/'>
           Home
         </Link>
         {currentUser ? (
-          <div className="menu-link" onClick={() => auth.signOut()}>
+          <div className='menu-link' onClick={() => auth.signOut()}>
             Sign Out
           </div>
         ) : (
-          <Link className="menu-link" to="/plants-e-commerce/signin">
+          <Link className='menu-link' to='/plants-e-commerce/signin'>
             Sign In
           </Link>
         )}
-        <div className="menu-link" to="/plants-e-commerce/contacts">
+        <div className='menu-link' to='/plants-e-commerce/contacts'>
           <Cart />
         </div>
       </div>
